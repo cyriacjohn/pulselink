9fc1b51200f350f60afd4e635fc648db6f63e6e2
Cyriac John <yohancyriac2002@gmail.com>
2026-03-23 22:40:44 +0530

    fix

--- BDMS.Api/Program.cs
+++ BDMS.Api/Program.cs
@@ -115,8 +115,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy
         =>
     {
-        policy.WithOrigins("https://pulse-link.netlify.app").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
-    }
+        policy.WithOrigins("https://pulse-link.netlify.app").AllowAnyHeader().AllowAnyMethod().AllowCredentials();     
         );
 });
 var app = builder.Build();
@@ -159,7 +158,7 @@ if (app.Environment.IsDevelopment())
 
 //app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
-//app.UseMiddleware<ExceptionMiddleware>();
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseAuthentication();
 app.UseAuthorization();
 
